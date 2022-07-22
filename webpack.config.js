9af0e7011b7d8ac832ed8d84a40ddc0c372bffb7
cutefcc <cutefcc@126.com>
2022-07-22 09:59:22 +0800

    feat: notes

--- webpack.config.js
+++ webpack.config.js
@@ -12,33 +12,38 @@ module.exports = {
   //   },
   plugins: [new BundleAnalyzerPlugin()],
   optimization: {
+    // 提取runtime
     runtimeChunk: {
       name: "runtime",
     },
     splitChunks: {
       chunks: "all",
       cacheGroups: {
+        // 提取common
         commons: {
           chunks: "all",
           name: "chunk-common",
           minChunks: 2, // 最少被引用次数 >=  时生成公共代码块，一般设置为2 or 3
           maxInitialRequests: 5,
           priority: 0, // 设置优先级，越大越优先打包
+          // 实在不行还可以用cacheGroups de test 去匹配 utils 让utils 单独成包， 类似这样 test: /src[\\/]utils/
           enforce: true, // 这个打开才能吧 utils 提出来  告诉 webpack 忽略 splitChunks.minSize、splitChunks.minChunks、splitChunks.maxAsyncRequests 和 splitChunks.maxInitialRequests 选项，并始终为此缓存组创建 chunk。
           reuseExistingChunk: true,
         },
+        // 优先提取node_modules
         venders: {
           name: "venders",
           test: /[\\/]node_modules[\\/]/,
           priority: 1, // 设置优先级，越大越优先打包
         },
       },
-      // 生成 chunk 的最小体积（以 bytes 为单位）
+      // 生成 chunk 的最小体积（以 bytes 为单位） 经验值 30kb
       minSize: {
         javascript: 30000,
         style: 0,
       },
       // 告诉 webpack 尝试将大于 maxSize 个字节的 chunk 分割成较小的部分。 这些较小的部分在体积上至少为 minSize（仅次于 maxSize）。 该算法是确定性的，对模块的更改只会产生局部影响。这样，在使用长期缓存时就可以使用它并且不需要记录。
+      // maxSize 经验值120kb
       maxSize: {
         javascript: 120000,
         style: 0,
